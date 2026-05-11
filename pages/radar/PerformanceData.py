8f212251e66615fe953cfe9b8f25a6c68cc942c0
Nicolas Bertho <nicolasbertho1996@gmail.com>
2026-05-11 20:39:29 +0200

    Added aircraft performance data class

--- pages/radar/PerformanceData.py
+++ pages/radar/PerformanceData.py
@@ -6,16 +6,53 @@ class PerformanceData:
     path_to_perf_file = "\\pages\\radar\\data\\aircraft_performance.json"
 
     def __init__(self, root_dir: str):
+        self.wtc_default = "M"
         self.root_dir = root_dir
-        self.load_perf_file()
         self.data = {}
+        self.load_perf_file()
 
     def load_perf_file(self):
         file = self.root_dir + self.path_to_perf_file
         with open(file, 'r') as raw_data:
             data = json.load(raw_data)
             for item in data:
-                pass
+                name = item["ICAO"]
+                self.data[name] = {}
+                cur = self.data[name]
+                cur["WTC"] = item["WTC"]
+                cur["ceiling"] = item["ceiling"]
+                cur["max_speed"] = item["max_speed"]
+                cur["climb"] = []
+                cur["descent"] = []
+                for rule in item["alt_vars"]:
+                    if rule["cr"] == 1:
+                        cur["climb"].append({
+                            "min": rule["min"],
+                            "max": rule["max"],
+                            "roc": rule["roc"],
+                            "speed": rule["speed"],
+                        })
+                    elif rule["cr"] == -1:
+                        cur["descent"].append({
+                            "min": rule["min"],
+                            "max": rule["max"],
+                            "roc": rule["roc"],
+                            "speed": rule["speed"],
+                        })
+
+    def get_perf_by_icao(self, icao: str):
+        try:
+            return self.data[icao]
+        except KeyError:
+            return False
 
     def get_wtc(self, icao_type):
-        return "M"
+        perf = self.get_perf_by_icao(icao_type)
+        wtc = self.wtc_default
+        try:
+            if perf is not False:
+                wtc = perf["WTC"]
+        except ValueError:
+            pass
+        else:
+            return wtc
